5acb3da0287356c80905b59da0c0f0cf2e9c03b0
KONICCO <nico678514@gmail.com>
2025-03-04 16:43:49 +0700

    add: redux setup

---
+++ FE/src/lib/store.ts
@@ -0,0 +1,19 @@
+import { ApiSlice } from '@/services/base-query'
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+
+export const store = configureStore({
+  reducer: {
+    [ApiSlice.reducerPath]: ApiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(ApiSlice.middleware)
+    }
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
+
+setupListeners(store.dispatch)
